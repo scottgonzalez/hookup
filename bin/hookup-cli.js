deeedf47a3fa5c2fbd15a6f49ec1cb2959034425
Scott González <scott.gonzalez@gmail.com>
2013-03-04 23:03:51 -0500

    Binary: Added --list support for listing available events.

--- bin/hookup-cli.js
+++ bin/hookup-cli.js
@@ -3,6 +3,15 @@
 var hookup = require( "../lib/hookup.js" );
 var optimist = require( "optimist" );
 
+// Process --list manually so that we can mark the other fields as required
+if ( process.argv.indexOf( "--list" ) > -1 ) {
+	console.log( "The following hooks are available:" );
+	Object.keys( hookup.Client.events ).forEach(function( event ) {
+		console.log( "  " + event + " - " + hookup.Client.events[ event ] );
+	});
+	return;
+}
+
 var argv = optimist
 	.usage( "Easily configure GitHub web hooks." )
 	.options( "l", {
@@ -29,6 +38,9 @@ var argv = optimist
 		alias: "events",
 		demand: true,
 		describe: "List of events, comma or space delimited"
+	})
+	.options( "list", {
+		describe: "List all available events."
 	})
 	.argv;
 
